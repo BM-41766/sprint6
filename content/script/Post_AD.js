49fb636f488998a975cc929d3c0e29160533c39c
harishma <harishmahari1502gmail.com>
2017-07-12 12:25:06 +0530

    sprint6 submit

---
+++ content/script/Post_AD.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#body").ready(function () {
+        var status = $("#status").val();
+       // alert(status);
+        if(status==1)
+        {
+            $("#Postad").show();
+            alert("dhsfd");
+        }
+    })
+});
